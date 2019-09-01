1dc581f09a5b0c9fa2459bcdadc94ea0ea6828b0
Utente <Utente@DESKTOP-TTN64SK>
2019-09-01 14:37:40 +0200

    FiRST Commit

---
+++ Previsione_caricamento_dati_storici/src/main/java/it/roberto/Previsione_caricamento_dati_storici/App.java
@@ -0,0 +1,13 @@
+package it.roberto.Previsione_caricamento_dati_storici;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
