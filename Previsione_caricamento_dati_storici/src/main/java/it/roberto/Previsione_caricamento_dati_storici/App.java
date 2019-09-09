202fcb347f38a83e205c08096192101db396d842
RobyPad <RobyPad@users.noreply.github.com>
2019-09-10 01:25:36 +0200

    Scrittura funzionante

--- Previsione_caricamento_dati_storici/src/main/java/it/roberto/Previsione_caricamento_dati_storici/App.java
+++ Previsione_caricamento_dati_storici/src/main/java/it/roberto/Previsione_caricamento_dati_storici/App.java
@@ -1,8 +1,12 @@
 package it.roberto.Previsione_caricamento_dati_storici;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
+import com.roberto.caricamento.dati.CaricaPartite;
 
 import it.roberto.controller.PartitaController;
 import it.roberto.model.Partita;
@@ -15,17 +19,41 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+		// TODO Auto-generated method stub
+		
+		/*
+		 * Italia serie A 2016/2017
+		 * 
+		 */
+		String url_italia = "https://www.diretta.it/serie-a-2016-2017/";
+		String href_italia = "//a[@href='/serie-a-2016-2017/risultati/']";
+	    
+		
+		CaricaPartite o = new CaricaPartite();
+		
+		//o.caricaCampionato(url_francia,href_francia);
+		
+		List<Partita> listaPartite = o.caricaCampionato3(url_italia, href_italia);
         
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("it.roberto.persistence");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
         
 		entityManager.getTransaction().begin();
-		Partita partita = new Partita("2019_08_30_Roma_Lazio", "italia", "Serie A", "1 Giornata", "2019/2019", "30.08", "20.45", "Roma", 1, "Lazio", 1);
-		entityManager.persist(partita);
+		
+		for (Partita partita : listaPartite) 
+		{
+			//System.out.println(partita);
+			entityManager.persist(partita);
+
+		}
+		
 		entityManager.getTransaction().commit();
 		entityManager.close();
-		PartitaController.close();		
+		PartitaController.close();
+		
+		//Partita partita = new Partita("2019_08_30_Roma_Lazio", "italia", "Serie A", "1 Giornata", "2019/2019", "30.08", "20.45", "Roma", 1, "Lazio", 1);
+
+		
 
 		System.out.println("Entity saved.");
     }
